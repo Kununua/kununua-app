7623cb3ba6f6a85473eb86146e061088a2301ab0
Alex-GF <yalejandro9@gmail.com>
2023-02-10 11:15:09 +0100

    FIX: SCRAPER EL JAMON

--- kununua_backend/scraper/models.py
+++ kununua_backend/scraper/models.py
@@ -3,24 +3,24 @@ from products.models import Category, Supermarket
 
 class ProductScraped(object):
     
-    def __init__(self, name=None, price=None, brand=None, image=None, offer_price=None, 
-                 weight=None, is_vegetarian=False, is_gluten_free=False, 
+    def __init__(self, name=None, price=None, unit_price=None, brand=None, image=None, offer_price=None, 
+                 is_vegetarian=False, is_gluten_free=False, 
                  is_freezed=False, is_from_country=False, is_eco=False, 
-                 is_without_sugar=False, is_without_lactose=False, category=None, 
+                 is_without_sugar=False, is_without_lactose=False, url=None, category=None, 
                  supermarket=None):
         
         if type(name) is not str:
             raise ValueError("Product name cannot be None and must be a string")
         if type(price) is not float:
             raise ValueError("Product price cannot be None and must be a float")
+        if type(unit_price) is not str:
+            raise ValueError("Product unit_price cannot be None and must be a string")
         if type(brand) is not str:
             raise ValueError("Product brand cannot be None and must be a string")
         if type(image) is not str:
             raise ValueError("Product image cannot be None and must be a string")
         if offer_price is not None and type(offer_price) is not float:
             raise ValueError("Product offer_price must be None or float")
-        if weight is not None and type(weight) is not str:
-            raise ValueError("Product weight must be None or string")
         if type(is_vegetarian) is not bool:
             raise ValueError("Product is_vegetarian cannot be None and must be a boolean")
         if type(is_gluten_free) is not bool:
@@ -35,6 +35,8 @@ class ProductScraped(object):
             raise ValueError("Product is_without_sugar cannot be None and must be a boolean")
         if type(is_without_lactose) is not bool:
             raise ValueError("Product is_without_lactose cannot be None and must be a boolean")
+        if type(url) is not str:
+            raise ValueError("Product url cannot be None and must be a string")
         if type(category) is not Category:
             raise ValueError("Product category cannot be None and must be a Category object")
         if type(supermarket) is not Supermarket:
@@ -43,10 +45,10 @@ class ProductScraped(object):
         
         self.name = name
         self.price = price
+        self.unit_price = unit_price
         self.brand = brand
         self.image = image
         self.offer_price = offer_price
-        self.weight = weight
         self.is_vegetarian = is_vegetarian
         self.is_gluten_free = is_gluten_free
         self.is_freezed = is_freezed
@@ -54,8 +56,9 @@ class ProductScraped(object):
         self.is_eco = is_eco
         self.is_without_sugar = is_without_sugar
         self.is_without_lactose = is_without_lactose
+        self.url = url
         self.category = category
         self.supermarket = supermarket
         
     def __str__(self):
-        return f"Product[name: {self.name}, price: {self.price}, brand: {self.brand}, offer_price: {self.offer_price}, weight: {self.weight}, category: {self.category.name}, supermarket: {self.supermarket.name}]"
+        return f"Product[name: {self.name}, price: {self.price}, unit_price: {self.unit_price}, brand: {self.brand}, offer_price: {self.offer_price}, category: {self.category.name}, supermarket: {self.supermarket.name}]"
